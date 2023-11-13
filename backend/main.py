1881d59ee1c9b380b9463d7ec65ecd6e0ee05d39
pcr9x <petchppw@gmail.com>
2023-11-13 16:55:21 +0700

    feature(backend): remove some fields, add pic

--- backend/main.py
+++ backend/main.py
@@ -7,24 +7,6 @@ import transaction
 
 app = FastAPI()
 
-# Define a context manager for the ZODB connection
-@contextmanager
-def zodb_connection():
-    storage = FileStorage('mydata.fs')
-    db = DB(storage)
-    conn = db.open()
-    root = conn.root()
-    try:
-        yield conn, root
-    finally:
-        transaction.commit()
-        conn.close()
-        db.close()
-
 # Include your routers
 app.include_router(login.router)
 app.include_router(booking.router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
--- backend/routers/login.py
+++ backend/routers/login.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI, HTTPException, APIRouter
+from fastapi import FastAPI, HTTPException, APIRouter, File, UploadFile, Query, Form
 from pydantic import BaseModel
 from ZODB import DB
 from ZODB.FileStorage import FileStorage
 from persistent import Persistent
 from passlib.context import CryptContext
-import transaction
+from typing import Optional
+import transaction, base64, uuid, os
 
 router = APIRouter()
 
 # Password hashing configuration
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 class UserCreate(BaseModel):
     email: str
@@ -17,34 +19,44 @@ class UserCreate(BaseModel):
     firstname: str
     lastname: str
     ID: str
-    year: str
-    faculty: str
-    department: str
+    year_of_study: str
+    profile_picture: Optional[str] = None
+
 
 class UserLogin(BaseModel):
     email: str
     password: str
+
 
 class UserData(BaseModel):
     email: str
     firstname: str
     lastname: str
     ID: str
-    year: str
-    faculty: str
-    department: str
+    year_of_study: str
+    profile_picture: Optional[str] = None
+
 
 # Define a UserDB class as a persistent object
 class UserDB(Persistent):
-    def __init__(self, email, password, firstname, lastname, ID, year, faculty, department):
+    def __init__(
+        self,
+        email,
+        password,
+        firstname,
+        lastname,
+        ID,
+        year_of_study,
+        profile_picture=None,
+    ):
         self.email = email
         self.password = password
         self.firstname = firstname
         self.lastname = lastname
         self.ID = ID
-        self.year = year
-        self.faculty = faculty
-        self.department = department
+        self.year_of_study = year_of_study
+        self.profile_picture = profile_picture
+
 
 LOGIN_INFO = {"isLogin": False, "user": ""}
 
@@ -53,11 +65,19 @@ db = DB(storage)
 connection = db.open()
 root = connection.root()
 
-def register_user(user: UserCreate):
-    if user.email in root:
+user_register = {
+    "email": None,
+    "password": None,
+}
+
+
+@router.post("/auth/register/identifier", response_model=dict)
+async def register_email(email_data: dict):
+    email = email_data["email"]
+    if email in root:
         return {"message": "Email already exists"}
 
-    user_email = user.email.split("@")
+    user_email = email.split("@")
     if (
         user_email[1] != "kmitl.ac.th"
         or not user_email[0].isdigit()
@@ -65,48 +85,104 @@ def register_user(user: UserCreate):
         or len(user_email) != 2
     ):
         return {"message": "Requires KMITL email only"}
+    user_register["email"] = email
+    return {"message": "Email is valid"}
 
+
+@router.post("/auth/register/password", response_model=dict)
+async def register_password(password_data: dict):
+    password = password_data["password"]
     # Hash the password before storing it
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = pwd_context.hash(password)
+    user_register["password"] = hashed_password
 
-    # Create a unique key for each user (e.g., using email as the key)
+    return {"message": "Password is valid"}
+
+
+UPLOAD_FOLDER = "profileImages"
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+# Function to save the uploaded file to a specific location within a subdirectory named after the user's ID
+def save_uploaded_file(contents, filename, user_id):
+    user_directory = os.path.join(UPLOAD_FOLDER, user_id)
+    if not os.path.exists(user_directory):
+        os.makedirs(user_directory)
+
+    file_path = os.path.join(user_directory, filename)
+    with open(file_path, "wb") as new_file:
+        new_file.write(contents)
+
+    return file_path
+
+
+@router.post("/auth/register/user-details", response_model=dict)
+async def register_user_details(
+    firstname: str = Form(...),
+    lastname: str = Form(...),
+    ID: str = Form(...),
+    year_of_study: str = Form(...),
+    profile_picture: UploadFile = File(...),
+):
+    contents = await profile_picture.read()
+    unique_filename = str(uuid.uuid4()) + ".jpeg"
+    file_path = save_uploaded_file(contents, unique_filename, ID)
+
     user_db = UserDB(
-        email=user.email,
-        password=hashed_password,
-        firstname=user.firstname,
-        lastname=user.lastname,
-        ID=user.ID,
-        year=user.year,
-        faculty=user.faculty,
-        department=user.department,
+        email=user_register["email"],
+        password=user_register["password"],
+        firstname=firstname,
+        lastname=lastname,
+        ID=ID,
+        year_of_study=year_of_study,
+        profile_picture=file_path,
     )
-    root[user.email] = user_db
+    root[user_register["email"]] = user_db
     transaction.commit()
     return {"message": "User registered successfully"}
 
-@router.post("/register", response_model=dict)
-async def register(user: UserCreate):
-    return register_user(user)
 
-@router.post("/login", response_model=dict)
-async def login(user: UserLogin):
-    user_in_db = root.get(user.email)
+user_login = {
+    "email": None,
+    "password": None,
+}
+IS_VALID_EMAIL = False
 
-    if user_in_db and pwd_context.verify(user.password, user_in_db.password):
-        LOGIN_INFO["isLogin"] = True
-        LOGIN_INFO["user"] = UserCreate(
-            email=user_in_db.email,
-            password=user_in_db.password,
-            firstname=user_in_db.firstname,
-            lastname=user_in_db.lastname,
-            ID=user_in_db.ID,
-            year=user_in_db.year,
-            faculty=user_in_db.faculty,
-            department=user_in_db.department,
-        )
-        return {"message": "Login successful"}
-    
-    return {"message": "Login failed"}
+
+@router.post("/auth/login/identifier")
+async def is_valid_email(email_data: dict):
+    global IS_VALID_EMAIL
+    email = email_data["email"]
+    user_in_db = root.get(email)
+    if user_in_db:
+        IS_VALID_EMAIL = True
+        user_login["email"] = email
+        return {"message": "Email is valid"}
+    return {"message": "Email not found"}
+
+
+@router.post("/auth/login/password", response_model=dict)
+async def is_valid_password(password_data: dict):
+    password = password_data["password"]
+    print(IS_VALID_EMAIL)
+    if IS_VALID_EMAIL:
+        user_in_db = root.get(user_login["email"])
+        if user_in_db and pwd_context.verify(password, user_in_db.password):
+            LOGIN_INFO["isLogin"] = True
+            LOGIN_INFO["user"] = UserCreate(
+                email=user_in_db.email,
+                password=user_in_db.password,
+                firstname=user_in_db.firstname,
+                lastname=user_in_db.lastname,
+                ID=user_in_db.ID,
+                year_of_study=user_in_db.year_of_study,
+            )
+            return {"message": "Login successful"}
+        return {"message": "Password is incorrect"}
+    return {"message": "Email not found"}
+
 
 @router.put("/update_password", response_model=dict)
 async def update_password(request_data: UserLogin):
@@ -121,6 +197,7 @@ async def update_password(request_data: UserLogin):
         return {"message": "Password updated successfully"}
     return {"message": "User not found"}
 
+
 @router.get("/users/all", response_model=list[UserData])
 async def get_all_users():
     user_list = [
@@ -129,21 +206,30 @@ async def get_all_users():
             ID=user.ID,
             firstname=user.firstname,
             lastname=user.lastname,
-            year=user.year,
-            faculty=user.faculty,
-            department=user.department,
+            year_of_study=user.year_of_study,
+            profile_picture=user.profile_picture,
         )
         for user in root.values()
     ]
     return user_list
 
-@router.get("/users/{user_id}", response_model=dict)
+
+@router.get("/users/{user_id}", response_model=UserData)
 async def get_user_by_id(user_id: str):
-    for i in router:
-        user = router[i]
+    for user_key in root.keys():
+        user = root[user_key]
         if user.ID == user_id:
-            return user.dict()
+            user_data = UserData(
+                email=user.email,
+                firstname=user.firstname,
+                lastname=user.lastname,
+                ID=user.ID,
+                year_of_study=user.year_of_study,
+                profile_picture=user.profile_picture,
+            )
+            return user_data
     return {"message": "User not found"}
+
 
 @router.get("/logout", response_model=dict)
 async def logout():
